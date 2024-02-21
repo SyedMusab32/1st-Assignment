a2e5b99fe59f5770ccfd186b290793a0074acee2
SyedMusab32 <160223970+SyedMusab32@users.noreply.github.com>
2024-02-21 18:46:09 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// Assignment1 (Q1)
+// console.log("Hello World");
+// Assignment2 (Q2)
+// var myname = 'Syed Musab';
+// var message = 'is a nice person';
+// console.log(myname + " " + message);
---
+++ main.ts
@@ -0,0 +1,7 @@
+// Assignment1 (Q1)
+console.log("Hello World");
+
+// Assignment2 (Q2)
+let myname = 'Syed Musab';
+let message = 'is a nice person';
+console.log(myname+" "+message);
